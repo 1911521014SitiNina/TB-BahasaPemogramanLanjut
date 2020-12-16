a4b21b089fd4ba8b15d0813809f0eab7d19f244f
1911521014SitiNina <73597123+1911521014SitiNina@users.noreply.github.com>
2020-12-16 22:29:47 +0700

    Delete koneksi.java

--- koneksi.java
+++
@@ -1,7 +0,0 @@
-package kasir;
-
-public class koneksi {
-	static final String DB_URL = "jdbc:mysql://localhost/tb_bpl?serverTimezone=Asia/Jakarta";
-	static final String USERNAME = "root";
-	static final String PASSWORD = "";	
-}
