ee616372a4295eccc9fa7fcd64ac16b6f680b6f3
1911521014SitiNina <73597123+1911521014SitiNina@users.noreply.github.com>
2020-12-16 22:31:07 +0700

    Delete KelolaBarang.java

--- KelolaBarang.java
+++
@@ -1,9 +0,0 @@
-package kasir;
-
-public interface KelolaBarang {
-	public void TambahBarang() throws Exception;
-	public void CariBarang() throws Exception;
-	public void UbahBarang() throws Exception;
-	public void HapusBarang() throws Exception;
-	public void LihatData() throws Exception;
-}
