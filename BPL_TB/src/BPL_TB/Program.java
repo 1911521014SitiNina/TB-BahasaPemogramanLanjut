a2d813cdce743b6140c5b1cef9ee7c6bf0a76a7f
user <user@LAPTOP-7DN7UBMS>
2020-12-21 00:11:10 +0700

    Buat Project Java

---
+++ BPL_TB/src/BPL_TB/Program.java
@@ -0,0 +1,26 @@
+package BPL_TB;
+import java.util.Scanner;
+import java.util.Date;
+import java.util.InputMismatchException;
+public class Program 
+{
+	static Scanner scn = new Scanner(System.in);
+	static User user = new User();
+	static Date date = new Date();
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		int pilihan; 
+		
+		switch (Login.landingPage()) {
+		case 1:
+			user.login();
+			break;
+
+		case 2:
+			user.TambahAkun();
+			break;
+		}
+	}
+
+}
