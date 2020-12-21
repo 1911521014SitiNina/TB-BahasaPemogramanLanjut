072007bbb93915f75fe828087a8309dccd703b19
afdalhalimamda <73575111+afdalhalimamda@users.noreply.github.com>
2020-12-22 03:22:46 +0700

    Add files via upload

---
+++ BPL_TB/src/BPL_TB/DataTransaksi.java
@@ -0,0 +1,29 @@
+package olahbarang;
+
+public class DataTransaksi {
+		public void getAll() {
+				
+			}
+			public void skuinput() {
+				
+			}
+			public void inputprocess() {
+				
+			}
+			public void Jumlahbarang() {
+				
+			}
+			public void jmlprocess() {
+				
+			}
+			public void hasilorder() {
+				
+			}
+			public void input() {
+				
+			}
+			public void show() {
+				
+			}
+
+}
