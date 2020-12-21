d42ba0be7a54803bca288f004ea60b6a391de9e6
AnnishaAprillya <73475038+AnnishaAprillya@users.noreply.github.com>
2020-12-22 02:55:42 +0700

    Add files via upload

---
+++ BPL_TB/src/BPL_TB/OlahBarang.java
@@ -0,0 +1,9 @@
+package olahbarang;
+
+public interface OlahBarang {
+	public void TambahBarang() throws Exception;
+	public void CariBarang() throws Exception;
+	public void UbahBarang() throws Exception;
+	public void HapusBarang() throws Exception;
+	public void LihatData() throws Exception;
+}
